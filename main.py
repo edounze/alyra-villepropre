88cca8ad15e8c444000cc66e9955a72297925389
Charles <charles@charlesen.fr>
2024-03-20 18:34:04 +0100

    Mise en place de la détection d'une bouteille

--- main.py
+++ main.py
@@ -1,32 +1,59 @@
-# Doc
-# Caractéristiques pseudo-Haar : https://fr.wikipedia.org/wiki/Caract%C3%A9ristiques_pseudo-Haar
-# import librairies
 import cv2
+import numpy as np
+import tensorflow as tf
 
+# Chargement du modèle pré-entraîné SSD MobileNet V2
+model = tf.saved_model.load('data/saved_model')
+
+# Obtenir la fonction d'inférence à partir des signatures du modèle
+infer = model.signatures['serving_default']
+
+# Initialiser la capture vidéo
 cap = cv2.VideoCapture(0)
-print("well imported.....")
-# we will start by trying the eyeglass HaarCascade classifier
-print("initializing the face classifier")
-
-face = cv2.CascadeClassifier('haarcascade_garbage.xml')
 
 while True:
-    # read the image from the cam
-    _, image = cap.read()
-    # now i convert the image into grayscale
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, image = cap.read()
+    if not ret:
+        break
     
-    # detect all faces in the image
-    faces = face.detectMultiScale(image_gray, 1.3, 5)
-    # for every face draw a rectangle
-    for x, y, width, height in faces:
-        cv2.rectangle(image, (x, y), (x + width, y + height), color=(255, 0, 0), thickness=1)
-        
-    # cv2.putText(image,'Face',(10,500),font,1,(0,0,255),2)
-    cv2.imshow('Stark', image)
-    if cv2.waitKey(1) == ord('q'):
+    # Convertir l'image en RGB (OpenCV utilise BGR par défaut)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    # Préparer le tenseur d'entrée sans convertir en float32 cette fois
+    input_tensor = tf.convert_to_tensor([image_rgb], dtype=tf.uint8)
+
+    # Utiliser la signature d'inférence avec les entrées nommées correctement
+    # Note: Vous devez adapter le code ci-dessous selon les attentes de votre modèle spécifique.
+    #       Ici, nous utilisons une approche générique, mais les noms des entrées doivent correspondre à ceux définis dans la signature du modèle.
+    output_dict = infer(tf.constant(input_tensor))
+    
+    # Extraire les boîtes de détection, les scores, et les classes
+    # Note: Ajustez les clés d'accès aux résultats selon la sortie de votre modèle.
+    boxes = output_dict['detection_boxes'].numpy()[0]
+    scores = output_dict['detection_scores'].numpy()[0]
+    classes = output_dict['detection_classes'].numpy()[0].astype(np.int32)
+
+    height, width, _ = image.shape
+
+    # Boucler sur chaque détection
+    for i in range(boxes.shape[0]):
+        if scores[i] > 0.5:  # Seuil de confiance
+            # La classe 44 correspond à "bouteille" dans le modèle COCO
+            if classes[i] == 44:
+                box = boxes[i]
+                y_min, x_min, y_max, x_max = box
+                x_min, x_max, y_min, y_max = (x_min * width, x_max * width,
+                                              y_min * height, y_max * height)
+                x_min, x_max, y_min, y_max = int(x_min), int(x_max), int(y_min), int(y_max)
+
+                # Dessiner un rectangle autour de la bouteille
+                cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
+
+    # Afficher l'image
+    cv2.imshow('Detection de bouteilles en plastique', image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# releasing the videos
 cap.release()
 cv2.destroyAllWindows()
